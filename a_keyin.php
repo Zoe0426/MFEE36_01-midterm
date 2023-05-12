d5da4cb4596d78fbd3be782d10d31c90180e5d69
JennyMac <whoiskenny0408@gmail.com>
2023-05-12 14:32:51 +0800

    act_firstCommit

---
+++ a_keyin.php
@@ -0,0 +1,11 @@
+<?php include './parts/html-head.php' ?>
+<?php include './parts/navbar.php' ?>
+<h2>這邊新增東西</h2>
+
+
+<?php include './parts/script.php' ?>
+<script>
+
+
+</script>
+<?php include './parts/html-foot.php' ?>
