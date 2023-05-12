9007be30b6c1e922bd6a29b588177a70b3e0afbb
Zoe0426 <90545407+Zoe0426@users.noreply.github.com>
2023-05-12 14:50:47 +0800

    m_adminLogin add form

--- m_adminLogin.php
+++ m_adminLogin.php
@@ -1,10 +1,33 @@
-<?php include './parts/html-head.php' ?>
-<?php include './parts/navbar.php' ?>
+<?php include './partsNOEDIT/html-head.php' ?>
+<?php include './partsNOEDIT/navbar.php' ?>
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <div class="card">
+                <div class="card-body">
+                    <form name="form1" onsubmit="ckeckForm(event)">
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="text" class="form-control" id="email" name="email" data-required="1">
+                            <div class="form-text"></div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="password" class="form-label">密碼</label>
+                            <input type="password" class="form-control" id="password" name="password" data-required="1">
+                            <div class="form-text"></div>
+                        </div>
+                        <div class="alert alert-danger" role="alert" id="infoBar" style="display:none"></div>
+                        <button type="submit" class="btn btn-primary">登入</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
-
-<?php include './parts/script.php' ?>
+<?php include './partsNOEDIT/script.php' ?>
 <script>
 
 
 </script>
-<?php include './parts/html-foot.php' ?>
+<?php include './partsNOEDIT/html-foot.php' ?>
