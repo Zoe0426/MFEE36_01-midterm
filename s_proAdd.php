93ffe7055ae77364588aed2c2b85f030c8a703db
Shuyi <shuyi041@gmail.com>
2023-05-12 19:53:17 +0800

    s_proAdd addspec & its-api

--- s_proAdd.php
+++ s_proAdd.php
@@ -9,20 +9,26 @@ $r_shopCat = $pdo->query($sql_shopCat)->fetchAll();
 $sql_shopSup = "SELECT distinct `sup_sid`, `sup_name` FROM `shop_sup` ORDER BY `sup_name`";
 $r_shopSup = $pdo->query($sql_shopSup)->fetchAll();
 
+//下拉的規格料表
+$sql_shopSpec = "SELECT distinct `spec_sid`, `spec_name` FROM `shop_spec` ORDER BY `spec_sid`";
+$r_shopSpec = $pdo->query($sql_shopSpec)->fetchAll();
 ?>
 <?php include './partsNOEDIT/html-head.php' ?>
 <style>
-    #s_proImgBox {
-        margin-top: 10px;
-        border: 2px dashed lightgray;
-        /* width: 500px; */
-        height: 430px;
+    .s_ImgBox {
         display: flex;
         justify-content: center;
         align-items: center;
         font-size: 50px;
         color: lightgray;
         position: relative;
+    }
+
+    #s_proImgBox {
+        margin-top: 10px;
+        border: 2px dashed lightgray;
+        /* width: 500px; */
+        height: 430px;
     }
 
     #s_Form1,
@@ -38,6 +44,12 @@ $r_shopSup = $pdo->query($sql_shopSup)->fetchAll();
         display: none;
         position: absolute;
     }
+
+
+    #s_proDetImgBox {
+        height: 190px;
+        border: 1px dashed lightgray;
+    }
 </style>
 
 <?php include './partsNOEDIT/navbar.php' ?>
@@ -47,12 +59,14 @@ $r_shopSup = $pdo->query($sql_shopSup)->fetchAll();
         <form name="s_Form1" id="s_Form1">
             <input type="file" name="shopTepProImg" accept="image/jpeg" id="shopTepProImg">
         </form>
-        <form name="s_Form2" return flase>
+        <form class="pt-4" name="s_Form2" return flase>
             <h2>新增商品</h2>
-            <div class="d-flex align-items-start">
-                <div class="w-25 me-3" onclick="shopAddMainImg()" id="s_proImgBox"><img src="" id="s_imginfo">+</div>
-                <input type="text" name="pro_img" id="s_proImg">
-                <div class="w-50">
+            <div class="row pb-3 border-bottom">
+                <div class="col-5">
+                    <div class="w-100 s_ImgBox" onclick="shopAddMainImg()" id="s_proImgBox"><img src="" id="s_imginfo">+</div>
+                    <input type="text" name="pro_img" id="s_proImg">
+                </div>
+                <div class="col-7">
                     <div class="mb-3">
                         <label class="form-label" for="pro_name">產品名稱</label>
                         <input type="text" class="form-control" id="pro_name" name="pro_name">
@@ -82,11 +96,9 @@ $r_shopSup = $pdo->query($sql_shopSup)->fetchAll();
                             <div class="col-6">
                                 <select class="form-select" name="catDet_sid" id="shopCatDetSel"></select>
                             </div>
-
                         </div>
                         <div class="form-text"></div>
                     </div>
-
                     <div class="mb-3">
                         <label class="form-label" for="s_sup_sid">供應商</label>
                         <div class="row">
@@ -107,6 +119,71 @@ $r_shopSup = $pdo->query($sql_shopSup)->fetchAll();
                     <div class="mb-3">
                         <label for="pro_describe" class="form-label">產品描述</label>
                         <textarea name="pro_describe" id="pro_describe" class="form-control"></textarea>
+                        <div class="form-text"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="row pb-3 border-bottom mt-4">
+                <div class="col-3 mb-3">
+                    <!-- 要更改function -->
+                    <div>
+                        <div class="s_ImgBox" onclick="shopAddMainImg()" id="s_proDetImgBox"><img src="" id="s_imginfo">+</div>
+                        <input type="text" name="pro_img" id="s_proImg">
+                    </div>
+                    <div class="mt-3">
+                        <label class="form-label">規格一</label>
+                        <div class="row s_spec">
+                            <div class="col-6">
+                                <select class="form-select s_spec_sid" name="spec_sid1[]">
+                                    <?php foreach ($r_shopSpec as $r) : ?>
+                                        <option value="<?= $r['spec_sid'] ?>"><?= $r['spec_name'] ?></option>
+                                    <?php endforeach; ?>
+                                </select>
+                            </div>
+
+                            <script>
+                                const shopSpecSel = document.querySelectorAll('.s_spec_sid');
+                                const shopSpec = document.querySelectorAll('.s_spec');
+                                for (let a = 0, amax = shopSpecSel.length; a < amax; a++) {
+                                    shopSpecSel[a].addEventListener('change', () => {
+                                        const selS = shopSpecSel[a].value;
+                                        SpecSel(selS, a);
+                                    });
+                                }
+
+                                function SpecSel(speSel, a) {
+                                    fetch(`s_proSep-api.php?spec_sid1=${speSel}`)
+                                        .then(r => r.json())
+                                        .then(obj => {
+
+
+                                            console.log(obj)
+                                            const thedocFrag = document.createDocumentFragment();
+                                            const theDiv = document.createElement("div");
+                                            theDiv.classList.add('col-6');
+                                            const theSel = document.createElement('select');
+                                            theSel.classList.add('form-select', 's_specDet_sid');
+                                            theSel.setAttribute("name", "specDet_sid1[]");
+                                            for (let b = 0, bmax = obj.length; b < bmax; b++) {
+
+                                                theOpt = document.createElement('option');
+                                                theOpt.setAttribute("value", obj[b].specDet_sid);
+                                                theTxt = document.createTextNode(obj[b].specDet_name);
+                                                theOpt.append(theTxt);
+                                                thedocFrag.append(theOpt);
+                                            }
+                                            theSel.append(thedocFrag);
+                                            theDiv.append(theSel);
+                                            shopSpec[a].append(theDiv)
+
+                                        })
+                                }
+                                SpecSel(1, 0)
+                            </script>
+                        </div>
+
+
+
                         <div class="form-text"></div>
                     </div>
                 </div>
@@ -157,8 +234,8 @@ $r_shopSup = $pdo->query($sql_shopSup)->fetchAll();
     const shopCatSel = document.querySelector('#shopCatSel');
     const shopCatDetSel = document.querySelector('#shopCatDetSel');
     shopCatSel.addEventListener("change", () => {
-        $selC = shopCatSel.value
-        selShopCat($selC)
+        const selC = shopCatSel.value
+        selShopCat(selC)
     });
 
     function selShopCat(catSel) {
