a9b682510603423d20b458cfea0e957c4d4726e6
Shuyi <shuyi041@gmail.com>
2023-05-12 14:49:42 +0800

    s_push two api

---
+++ s_upLoadProImg-api.php
@@ -0,0 +1,20 @@
+<?php
+header('Content-Type: application/json');
+
+$output = [
+    'filename' => '',
+    'files' => $_FILES,       //除錯用
+];
+// print_r($_FILES);
+
+
+if (!empty($_FILES['shopTepProImg'])) {
+    $filename = sha1($_FILES['shopTepProImg']['name'] . uniqid()) . 'jpg';
+    if (move_uploaded_file($_FILES['shopTepProImg']['tmp_name'], "./shopImgs/$filename")) {
+        $output['filename'] = $filename;
+    } else {
+        $output['error'] = '移動檔案時發生錯誤';
+    }
+};
+header('Content-Type: application/json');
+echo json_encode($output, JSON_UNESCAPED_UNICODE);
