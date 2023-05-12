314d6c09c02763c1fd6435aa8ca3009f30a428db
Zoe0426 <90545407+Zoe0426@users.noreply.github.com>
2023-05-12 16:00:10 +0800

    login json 錯誤

--- m_adminLogin-api.php
+++ m_adminLogin-api.php
@@ -1,39 +1,37 @@
 <?php
+
 require './partsNOEDIT/connect-db.php';
 
 $output = [
-    'success' => false, #新增資料成功或失敗的結果（MUST）
+    'success' => false,
     'postData' => $_POST, # 除錯用的
     'code' => 0,
-    'error' => [],
 ];
 
 
-if (!empty($_POST['XXX'])) { #若附合此條件，則可以往下走
 
-    $isPass = true;
-    # TODO：檢查欄位資料,判斷格式錯，isPass設為false （MUST）
-    # TODO：整理變數，轉換資料格式
-
-    $sql = "INSERT INTO 資料表(
-    欄位們, 現在時間
-    ) VALUES (
-    ?,  NOW()
-    )"; #(你的sql)
-
+if (!empty($_POST['account']) and !empty($_POST['password'])) {
+    $sql = "SELECT * FROM mem_admin WHERE account=?";
     $stmt = $pdo->prepare($sql);
 
-    if ($isPass) { #以上所有條件成立，add到資料庫
-        $stmt->execute([
-            #變數或固定資料
-        ]);
+    $stmt->execute([
+        $_POST["account"]
+    ]);
 
-        $output['success'] = !!$stmt->rowCount(); #若加成功，$output訊息的success會顯示true
+    $row = $stmt->fetch();
+
+    if (empty($row)) {
+        $output['code'] = 410; #帳號錯誤
+    } else {
+        if ($row['password']) {
+            #密碼正確
+            $_SESSION['admin'] = $row;
+            $output['success'] = true;
+        } else {
+            #密碼錯誤
+            $output['code'] = 420;
+        }
     }
-} else {
-    $output['error'] = "xxx"; #不符合條件
 }
-
-
 header('Content-Type: application/json');
 echo json_encode($output, JSON_UNESCAPED_UNICODE);
--- m_adminLogin.php
+++ m_adminLogin.php
@@ -1,22 +1,27 @@
 <?php include './partsNOEDIT/html-head.php' ?>
 <?php include './partsNOEDIT/navbar.php' ?>
+<style>
+
+</style>
 <div class="container">
-    <div class="row">
+    <div class="row d-flex justify-content-center">
         <div class="col-6">
             <div class="card">
                 <div class="card-body">
-                    <form name="form1" onsubmit="ckeckForm(event)">
+                    <h5 class="card-title">管理員登入</h5>
+                    <form name="form1" onsubmit="checkForm(event)">
                         <div class="mb-3">
-                            <label for="email" class="form-label">Email</label>
-                            <input type="text" class="form-control" id="email" name="email" data-required="1">
+                            <label for="account" class="form-label">帳號</label>
+                            <input type="text" class="form-control" id="account" aria-describedby="emailHelp" name="account">
                             <div class="form-text"></div>
                         </div>
                         <div class="mb-3">
                             <label for="password" class="form-label">密碼</label>
-                            <input type="password" class="form-control" id="password" name="password" data-required="1">
+                            <input type="password" class="form-control" id="password" name="password">
                             <div class="form-text"></div>
                         </div>
-                        <div class="alert alert-danger" role="alert" id="infoBar" style="display:none"></div>
+                        <div class="alert alert-danger" role="alert" style="display:none">
+                        </div>
                         <button type="submit" class="btn btn-primary">登入</button>
                     </form>
                 </div>
@@ -27,7 +32,18 @@
 
 <?php include './partsNOEDIT/script.php' ?>
 <script>
+    function checkForm() {
+        event.preventDefault();
 
+        const fd = new FormData(document.form1);
+        fetch('m_adminLogin.php', {
+                method: 'POST',
+                body: fd,
+            }).then(r => r.json())
+            .then(obj => {
 
+            })
+    }
 </script>
+
 <?php include './partsNOEDIT/html-foot.php' ?>
