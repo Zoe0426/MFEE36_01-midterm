5dff707daadd81315dd317351d4e97a1bc94fc3e
Shuyi <shuyi041@gmail.com>
2023-05-13 22:53:18 +0800

    s_proAdd_big_change for many cat

--- s_proAdd.php
+++ s_proAdd.php
@@ -5,14 +5,29 @@ require './partsNOEDIT/connect-db.php';
 $sql_shopCat = "SELECT distinct `cat_sid`, `cat_name` FROM `shop_cat` ORDER BY `cat_name`";
 $r_shopCat = $pdo->query($sql_shopCat)->fetchAll();
 
+//下拉的子類別列表
+$sql_shopCatDet = "SELECT * FROM `shop_cat` ORDER BY `catDet_num` ";
+$r_shopCatDet = $pdo->query($sql_shopCatDet)->fetchAll();
+
 //下拉的供應商列表
 $sql_shopSup = "SELECT distinct `sup_sid`, `sup_name` FROM `shop_sup` ORDER BY `sup_name`";
 $r_shopSup = $pdo->query($sql_shopSup)->fetchAll();
 
-//下拉的規格料表
+//下拉的產地列表
+$sql_shopSupMIW = "SELECT * FROM `shop_sup`";
+$r_shopSupMIW = $pdo->query($sql_shopSupMIW)->fetchAll();
+
+//下拉的規格類別料表
 $sql_shopSpec = "SELECT distinct `spec_sid`, `spec_name` FROM `shop_spec` ORDER BY `spec_sid`";
 $r_shopSpec = $pdo->query($sql_shopSpec)->fetchAll();
+
+//下拉的各規格明細料表
+
+$sql_shopSpecDet = "SELECT * FROM `shop_spec`";
+$r_shopSpecDet = $pdo->query($sql_shopSpecDet)->fetchAll();
+
 ?>
+
 <?php include './partsNOEDIT/html-head.php' ?>
 <style>
     .s_ImgBox {
@@ -45,8 +60,7 @@ $r_shopSpec = $pdo->query($sql_shopSpec)->fetchAll();
         position: absolute;
     }
 
-
-    #s_proDetImgBox {
+    .s_proDetImgBox {
         height: 190px;
         border: 1px dashed lightgray;
     }
@@ -76,6 +90,7 @@ $r_shopSpec = $pdo->query($sql_shopSpec)->fetchAll();
                         <label class="form-label">適用對象</label>
                         <div class="d-flex">
                             <select class="form-select" name="pro_for" id="shopForSel">
+                                <option value="" selected disabled>--請選擇--</option>
                                 <option value="D">狗</option>
                                 <option value="C">貓</option>
                                 <option value="B">皆可</option>
@@ -87,14 +102,15 @@ $r_shopSpec = $pdo->query($sql_shopSpec)->fetchAll();
                         <label class="form-label" for="s_sup_sid">產品類別</label>
                         <div class="row">
                             <div class="col-6">
-                                <select class="form-select" name="cat_sid" id="shopCatSel">
+                                <select class="form-select" name="cat_sid" id="s_cat_sid">
+                                    <option value="" selected disabled>--請選擇--</option>
                                     <?php foreach ($r_shopCat as $r) : ?>
                                         <option value="<?= $r['cat_sid'] ?>"><?= $r['cat_name'] ?></option>
                                     <?php endforeach; ?>
                                 </select>
                             </div>
                             <div class="col-6">
-                                <select class="form-select" name="catDet_sid" id="shopCatDetSel"></select>
+                                <select class="form-select" name="catDet_sid" id="s_catDet_sid" disabled></select>
                             </div>
                         </div>
                         <div class="form-text"></div>
@@ -104,15 +120,15 @@ $r_shopSpec = $pdo->query($sql_shopSpec)->fetchAll();
                         <div class="row">
                             <div class="col-6">
                                 <select class="form-select" name="sup_sid" id="s_sup_sid">
+                                    <option value="" selected disabled>--請選擇--</option>
                                     <?php foreach ($r_shopSup as $r) : ?>
                                         <option value="<?= $r['sup_sid'] ?>"><?= $r['sup_name'] ?></option>
                                     <?php endforeach; ?>
                                 </select>
                             </div>
                             <div class="col-6">
-                                <select class="form-select" name="sup_MIW" id="s_sup_MIW"></select>
+                                <select class="form-select" name="sup_MIW" id="s_sup_MIW" disabled></select>
                             </div>
-
                         </div>
                         <div class="form-text"></div>
                     </div>
@@ -125,65 +141,57 @@ $r_shopSpec = $pdo->query($sql_shopSpec)->fetchAll();
             </div>
             <div class="row pb-3 border-bottom mt-4">
                 <div class="col-3 mb-3">
-                    <!-- 要更改function -->
-                    <div>
-                        <div class="s_ImgBox" onclick="shopAddMainImg()" id="s_proDetImgBox"><img src="" id="s_imginfo">+</div>
+                    <div class="mb-3">
+                        <div class="s_ImgBox s_proDetImgBox" onclick="shopAddMainImg()"><img src="" id="s_imginfo">+</div>
                         <input type="text" name="pro_img" id="s_proImg">
                     </div>
-                    <div class="mt-3">
+                    <div class="mb-3 s_spec">
                         <label class="form-label">規格一</label>
-                        <div class="row s_spec">
+                        <div class="row">
                             <div class="col-6">
-                                <select class="form-select s_spec_sid" name="spec_sid1[]">
-                                    <?php foreach ($r_shopSpec as $r) : ?>
-                                        <option value="<?= $r['spec_sid'] ?>"><?= $r['spec_name'] ?></option>
-                                    <?php endforeach; ?>
+                                <select class="form-select s_spec_sid1" name="spec_sid1[]">
+                                    <option value="" selected disabled>--請選擇--</option>
                                 </select>
                             </div>
-
-                            <script>
-                                const shopSpecSel = document.querySelectorAll('.s_spec_sid');
-                                const shopSpec = document.querySelectorAll('.s_spec');
-                                for (let a = 0, amax = shopSpecSel.length; a < amax; a++) {
-                                    shopSpecSel[a].addEventListener('change', () => {
-                                        const selS = shopSpecSel[a].value;
-                                        SpecSel(selS, a);
-                                    });
-                                }
-
-                                function SpecSel(speSel, a) {
-                                    fetch(`s_proSep-api.php?spec_sid1=${speSel}`)
-                                        .then(r => r.json())
-                                        .then(obj => {
-
-
-                                            console.log(obj)
-                                            const thedocFrag = document.createDocumentFragment();
-                                            const theDiv = document.createElement("div");
-                                            theDiv.classList.add('col-6');
-                                            const theSel = document.createElement('select');
-                                            theSel.classList.add('form-select', 's_specDet_sid');
-                                            theSel.setAttribute("name", "specDet_sid1[]");
-                                            for (let b = 0, bmax = obj.length; b < bmax; b++) {
-
-                                                theOpt = document.createElement('option');
-                                                theOpt.setAttribute("value", obj[b].specDet_sid);
-                                                theTxt = document.createTextNode(obj[b].specDet_name);
-                                                theOpt.append(theTxt);
-                                                thedocFrag.append(theOpt);
-                                            }
-                                            theSel.append(thedocFrag);
-                                            theDiv.append(theSel);
-                                            shopSpec[a].append(theDiv)
-
-                                        })
-                                }
-                                SpecSel(1, 0)
-                            </script>
+                            <div class="col-6">
+                                <select class="form-select s_specDet_sid1" name="specDet_sid1[]" disabled></select>
+                            </div>
                         </div>
-
-
-
+                        <div class="form-text"></div>
+                    </div>
+                    <div class="mb-3 s_spec">
+                        <label class="form-label">規格二</label>
+                        <div class="row">
+                            <div class="col-6">
+                                <select class="form-select s_spec_sid2" name="spec_sid2[]" disabled></select>
+                            </div>
+                            <div class="col-6">
+                                <select class="form-select s_specDet_sid2" name="specDet_sid2[]" disabled></select>
+                            </div>
+                        </div>
+                        <div class="form-text"></div>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label" for="s_proDet_price">價格</label>
+                        <input type="number" class="form-control" name="proDet_price[]">
+                        <div class="form-text"></div>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label" for="s_proDet_qty">數量</label>
+                        <input type="number" class="form-control" name="proDet_qty[]">
+                        <div class="form-text"></div>
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">適用年齡</label>
+                        <div class="d-flex">
+                            <select class="form-select" name="pro_forAge[]">
+                                <option value="" selected disabled>--請選擇--</option>
+                                <option value="1">幼年</option>
+                                <option value="2">成年</option>
+                                <option value="3">高齡</option>
+                                <option value="4">皆可</option>
+                            </select>
+                        </div>
                         <div class="form-text"></div>
                     </div>
                 </div>
@@ -201,6 +209,9 @@ $r_shopSpec = $pdo->query($sql_shopSpec)->fetchAll();
 
 <?php include './partsNOEDIT/script.php' ?>
 <script>
+    const theDocFrag = document.createDocumentFragment();
+
+
     //新增主照片+API
     const shopTepProImg = document.querySelector("#shopTepProImg");
 
@@ -230,60 +241,149 @@ $r_shopSpec = $pdo->query($sql_shopSpec)->fetchAll();
             })
     })
 
-    //產品子類別API+生成option
-    const shopCatSel = document.querySelector('#shopCatSel');
-    const shopCatDetSel = document.querySelector('#shopCatDetSel');
-    shopCatSel.addEventListener("change", () => {
-        const selC = shopCatSel.value
-        selShopCat(selC)
+    //==================清除結點==================
+    function removeChild(a) {
+        while (a.hasChildNodes()) {
+            a.remove(a.lastChild)
+        }
+    }
+
+
+    //==================產品子類別自動生成==================
+    const catSel = document.querySelector("#s_cat_sid");
+    catSel.addEventListener("change", () => {
+        const catSelId = catSel.value
+        createCatDet(catSelId)
     });
 
-    function selShopCat(catSel) {
-        fetch(`s_proCat-api.php?cat_sid=${catSel}`)
-            .then(r => r.json())
-            .then(obj => {
-                while (shopCatDetSel.hasChildNodes()) {
-                    shopCatDetSel.removeChild(shopCatDetSel.lastChild)
-                }
-                //console.log(obj)
-                for (let o of obj) {
-                    const theOp = document.createElement('option');
-                    theOp.setAttribute('value', o.catDet_sid);
-                    const theText = document.createTextNode(o.catDet_name);
-                    theOp.append(theText)
-                    shopCatDetSel.append(theOp);
-                }
-            })
+    function createCatDet(catSelId) {
+        const catDet = <?= json_encode($r_shopCatDet, JSON_UNESCAPED_UNICODE) ?>;
+        const catDetSel = document.querySelector('#s_catDet_sid')
+        removeChild(catDetSel)
+        catDetSel.removeAttribute('disabled')
+        for (let a of catDet) {
+            if (catSelId == a.cat_sid) {
+                const theOp = document.createElement('option');
+                theOp.setAttribute("value", a.catDet_sid);
+                const theTxt = document.createTextNode(a.catDet_name);
+                theOp.append(theTxt);
+                theDocFrag.append(theOp);
+            }
+        }
+        catDetSel.append(theDocFrag)
     }
-    selShopCat('G')
+    //createCatDet('G')
 
 
+    //==================供應商產地自動生成==================
+    const supSel = document.querySelector('#s_sup_sid')
+    supSel.addEventListener('change', () => {
+        const supSelId = supSel.value;
+        createSupMIW(supSelId)
+    })
 
-    //供應商產地API+生成option
-    const s_sup_sid = document.querySelector('#s_sup_sid');
-    const sup_MIW = document.querySelector('#s_sup_MIW');
-    s_sup_sid.addEventListener("change", () => {
-        $selS = s_sup_sid.value
-        selShopSup($selS)
-    });
-
-    function selShopSup(supSel) {
-        fetch(`s_proSup-api.php?sup_sid=${supSel}`)
-            .then(r => r.json())
-            .then(obj => {
-                while (sup_MIW.hasChildNodes()) {
-                    sup_MIW.removeChild(sup_MIW.lastChild)
-                }
-                //console.log(obj)
-                for (let o of obj) {
-                    const theOp = document.createElement('option');
-                    theOp.setAttribute('value', o.sup_MIW_sid);
-                    const theText = document.createTextNode(o.sup_MIW);
-                    theOp.append(theText)
-                    sup_MIW.append(theOp);
-                }
-            })
+    function createSupMIW(supSelId) {
+        const supMIW = <?= json_encode($r_shopSupMIW, JSON_UNESCAPED_UNICODE) ?>;
+        const supMIWSel = document.querySelector('#s_sup_MIW')
+        supMIWSel.removeAttribute('disabled')
+        removeChild(supMIWSel)
+        for (let a of supMIW) {
+            if (supSelId == a.sup_sid) {
+                const theOp = document.createElement('option');
+                theOp.setAttribute('value', a.sup_MIW_sid)
+                const theTxt = document.createTextNode(a.sup_MIW)
+                theOp.append(theTxt);
+                theDocFrag.append(theOp);
+            }
+        }
+        supMIWSel.append(theDocFrag)
     }
-    selShopSup(1)
+    //createSupMIW(1)
+    //====================================
+
+    //==================商品規格自動生成==================
+    const spec = <?= json_encode($r_shopSpec, JSON_UNESCAPED_UNICODE) ?>;
+    const specDet = <?= json_encode($r_shopSpecDet, JSON_UNESCAPED_UNICODE) ?>;
+    let specSel1 = document.querySelectorAll('.s_spec_sid1')
+    let specDetSel1 = document.querySelectorAll(".s_specDet_sid1")
+    let specSel2 = document.querySelectorAll('.s_spec_sid2')
+    let specDetSel2 = document.querySelectorAll(".s_specDet_sid2")
+    // console.log(spec)
+    for (let a = 0, amax = specSel1.length; a < amax; a++) {
+        specSel1[a].addEventListener('change', () => {
+            const specSelId = specSel1[a].value;
+            createSpecDet1(specSelId, a);
+            createSpec2(specSelId, a);
+        })
+        for (let b of spec) {
+            const theOp = document.createElement('option');
+            theOp.setAttribute('value', b.spec_sid)
+            const theTxt = document.createTextNode(b.spec_name);
+            theOp.append(theTxt)
+            theDocFrag.append(theOp)
+        }
+        specSel1[a].append(theDocFrag)
+    }
+
+    function createSpec2(specSelId, a) {
+        specSel2[a].removeAttribute('disabled')
+        while (specSel2[a].hasChildNodes()) {
+            specSel2[a].remove(specSel2[a].lastChild)
+        }
+        for (let b of spec) {
+            if (b.spec_sid != specSelId) {
+                const theOp = document.createElement('option');
+                theOp.setAttribute('value', b.spec_sid)
+                const theTxt = document.createTextNode(b.spec_name);
+                theOp.append(theTxt)
+                theDocFrag.append(theOp)
+            }
+        }
+        specSel2[a].append(theDocFrag)
+        for (let c = 0, cmax = specSel2.length; c < cmax; c++) {
+            specSel2[c].addEventListener('change', () => {
+                const specSelId = specSel2[c].value;
+                createSpecDet2(specSelId, c);
+            })
+        }
+    }
+
+    function createSpecDet2(specSelId, index) {
+        specDetSel2[index].removeAttribute('disabled');
+        removeChild(specDetSel2[index])
+        createSpecDet(specSelId)
+        specDetSel2[index].append(theDocFrag)
+    }
+
+    function createSpecDet1(specSelId, index) {
+        specDetSel1[index].removeAttribute('disabled');
+        removeChild(specDetSel1[index])
+        createSpecDet(specSelId)
+        specDetSel1[index].append(theDocFrag)
+    }
+
+    function createSpecDet(specSelId) {
+        const arr = [];
+        for (let a of specDet) {
+            if (specSelId == a.spec_sid) {
+                arr.push(a);
+            }
+        }
+        //若小規格有數字，則由小到大排序
+        arr.sort(function(a, b) {
+            let c = parseInt(a.specDet_name);
+            let d = parseInt(b.specDet_name);
+            return c - d
+        })
+
+        for (let b of arr) {
+            const theOp = document.createElement('option');
+            theOp.setAttribute('value', b.specDet_sid);
+            const theTxt = document.createTextNode(b.specDet_name);
+            theOp.append(theTxt)
+            theDocFrag.append(theOp)
+        }
+        return theDocFrag
+    }
 </script>
 <?php include './partsNOEDIT/html-foot.php' ?>
