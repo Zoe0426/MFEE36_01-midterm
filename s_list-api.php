fe1d4d12b67b29985c49720cb16778d4e1e3588f
Shuyi <shuyi041@gmail.com>
2023-05-16 05:17:01 +0800

    update

--- s_list-api.php
+++ s_list-api.php
@@ -22,7 +22,7 @@ if ($totalRows) {
     };
     $sql = sprintf('SELECT * FROM `shop_proDet` pd
     JOIN `shop_pro` p ON pd.`pro_sid`=p.`pro_sid` 
-    JOIN `shop_cat` c ON p.`cat_sid`=c.`cat_sid` and p.`catDet_sid` =c.`catDet_sid` WHERE p.`pro_status` !=3 LIMIT %s, %s', ($page - 1) * $perPage, $perPage);
+    JOIN `shop_cat` c ON p.`cat_sid`=c.`cat_sid` and p.`catDet_sid` =c.`catDet_sid` WHERE p.`pro_status` !=3 ORDER BY p.`pro_update` DESC LIMIT %s, %s', ($page - 1) * $perPage, $perPage);
     $rows = $pdo->query($sql)->fetchAll();
 };
 // header('Content-Type: application/json');
