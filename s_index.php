f120e0bd486f9aa55ebe43b2f3e9f57f3993f467
Shuyi <shuyi041@gmail.com>
2023-05-15 15:53:22 +0800

    update for the list

--- s_index.php
+++ s_index.php
@@ -6,13 +6,9 @@ require './partsNOEDIT/connect-db.php'
 ?>
 <?php include './partsNOEDIT/html-head.php' ?>
 <?php include './partsNOEDIT/navbar.php' ?>
-
-here~~~
-
+Here
 
 <?php include './partsNOEDIT/script.php' ?>
 <script>
-
-
 </script>
 <?php include './partsNOEDIT/html-foot.php' ?>
