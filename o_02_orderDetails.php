3d849179cce2119b500d6a9975f7ef3d9498e2af
Lily Kuo <ilwitulily0209@gmail.com>
2023-05-15 20:09:10 +0800

    o-ShowOrders OK

--- o_02_orderDetails.php
+++ o_02_orderDetails.php
@@ -20,7 +20,7 @@ require './partsNOEDIT/connect-db.php' ?>
                 <div class="col-4">
                     <select class="form-select" aria-label="Default select example" name="searchBy" onchange="searchm(event)">
                         <option selected value="1">訂單編號</option>
-                        <option selected value="2">姓名</option>
+                        <option value="2">姓名</option>
                         <option value="3">手機</option>
                         <option value="4">會員編號</option>
                     </select>
@@ -38,37 +38,9 @@ require './partsNOEDIT/connect-db.php' ?>
             <div class="col-10 o-mem-table"> </div>
         </div>
     </form>
-
     <div class="row">
         <div class="col-12 pt-3" id="oOrdersTable">
-            <!-- <table class="table table-striped ocd">
-                <thead>
-                    <tr>
-                        <th scope="col">訂單編號</th>
-                        <th scope="col">會員編號</th>
-                        <th scope="col">訂單狀態</th>
-                        <th scope="col">優惠券編號</th>
-                        <th scope="col">寄送方式</th>
-                        <th scope="col">寄送狀態</th>
-                        <th scope="col">訂單成立時間</th>
-                        <th scope="col"><i class="fa-regular fa-pen-to-square"></i></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                        <td>@mdo</td>
-                        <td><i class="fa-regular fa-pen-to-square"></i></td>
-                    </tr>
-                </tbody>
-            </table> -->
         </div>
-
     </div>
     <div class="row"></div>
 </div>
@@ -83,7 +55,7 @@ require './partsNOEDIT/connect-db.php' ?>
         const nameInput = document.getElementById('sbname');
         const mobileInput = document.getElementById('sbmobile');
         const memsidInput = document.getElementById('sbmemsid');
-
+        oOrdersTable.innerHTML = '';
         if (nameInput.value || mobileInput.value || memsidInput.value || orderSidInput) {
             const fd = new FormData(document.getElementById('oGetmem'));
             fetch('o-api02_1_getMemOrders.php', {
@@ -93,10 +65,8 @@ require './partsNOEDIT/connect-db.php' ?>
                 .then(obj => {
                     console.log(obj.getBy);
                     if (obj.getBy == 'orderSid') {
-                        // console.log("getby Order");
                         tableByOrderSid(obj);
                     } else {
-                        // console.log('getby other');
                         tableByMemInfo(obj);
                     }
                     console.log(obj);
@@ -156,12 +126,12 @@ require './partsNOEDIT/connect-db.php' ?>
     }
     // ====顯示指定訂單
     function tableByOrderSid(obj) {
-        console.log('display');
         const orderSidTable = document.createElement('div');
         let orderStatus = "";
         let post_Status = "";
+        let post_type = "";
         obj.order_status == 0 ? orderStatus = "未付款" : orderStatus = "已付款";
-
+        obj.postType == 1 ? post_type = "寄送到府" : post_type = "超市領取";
         if (obj.postStatus == 0) {
             post_Status = '已領取';
         } else if (obj.postStatus == 1) {
@@ -173,6 +143,7 @@ require './partsNOEDIT/connect-db.php' ?>
         } else if (obj.postStatus == 4) {
             post_Status = '未領取';
         }
+
         orderSidTable.innerHTML = `<table class="table table-striped ocd">
                 <thead>
                     <tr>
@@ -183,19 +154,19 @@ require './partsNOEDIT/connect-db.php' ?>
                         <th scope="col">寄送方式</th>
                         <th scope="col">寄送狀態</th>
                         <th scope="col">訂單成立時間</th>
-                        <th scope="col"><i class="fa-regular fa-pen-to-square"></i></th>
+                        <th scope="col"><i class="fa-regular fa-file-lines"></i></th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
                         <th scope="row">${obj.order_sid}</th>
-                        <td>${obj.sid}</td>
-                        <td>${obj.order_sid}</td>
+                        <td>${obj.member_sid}</td>
                         <td>${orderStatus}</td>
                         <td>${obj.coupon_sid}</td>
+                        <td>${post_type}</td>
                         <td>${post_Status}</td>
                         <td>${obj.createDt}</td>
-                        <td><i class="fa-regular fa-pen-to-square text-success"></i></td>
+                        <td><i class="fa-regular fa-file-lines text-success" onclick="showDetails(${obj.order_sid})"></i></td>
                     </tr>
                 </tbody>
             </table>`;
@@ -204,6 +175,67 @@ require './partsNOEDIT/connect-db.php' ?>
     }
     // ====顯示某員的所有訂單
     function tableByMemInfo(obj) {
+        const mixTable = document.createElement('div');
+        let displayItems;
+        if (obj.getBy == "memName") {
+            displayItems = obj.name_orders;
+        } else if (obj.getBy == "memMobile") {
+            displayItems = obj.mobile_orders;
+        } else if (obj.getBy == "memSid") {
+            displayItems = obj.sid_orders;
+        }
+
+        let orderStatus = "";
+        let post_Status = "";
+        let post_type = "";
+        let items;
+
+        for (let i = 0, len = displayItems.length; i < len; i++) {
+            displayItems[i].order_status == 0 ? orderStatus = "未付款" : orderStatus = "已付款";
+            displayItems[i].postType == 1 ? post_type = "寄送到府" : post_type = "超市領取";
+            if (displayItems[i].postStatus == 0) {
+                post_Status = '已領取';
+            } else if (displayItems[i].postStatus == 1) {
+                post_Status = '備貨中';
+            } else if (displayItems[i].postStatus == 2) {
+                post_Status = '運送中';
+            } else if (displayItems[i].postStatus == 3) {
+                post_Status = '貨到超商';
+            } else if (displayItems[i].postStatus == 4) {
+                post_Status = '未領取';
+            }
+            items += `<tr>
+                        <th scope="row">${displayItems[i].order_sid}</th>
+                        <td>${displayItems[i].member_sid}</td>
+                        <td>${orderStatus}</td>
+                        <td>${displayItems[i].coupon_sid}</td>
+                        <td>${post_type}</td>
+                        <td>${post_Status}</td>
+                        <td>${displayItems[i].createDt}</td>
+                        <td><i class="fa-regular fa-file-lines text-success" onclick="showDetails(${displayItems[i].order_sid})"></i></td>
+                    </tr>`
+        }
+        mixTable.innerHTML = `<table class="table table-striped ocd">
+                <thead>
+                    <tr>
+                        <th scope="col">訂單編號</th>
+                        <th scope="col">會員編號</th>
+                        <th scope="col">訂單狀態</th>
+                        <th scope="col">優惠券編號</th>
+                        <th scope="col">寄送方式</th>
+                        <th scope="col">寄送狀態</th>
+                        <th scope="col">訂單成立時間</th>
+                        <th scope="col"><i class="fa-regular fa-file-lines"></i></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    ${items}
+                </tbody>
+            </table>`;
+        oOrdersTable.append(mixTable);
+    }
+    //顯示訂單明細
+    function showDetails() {
 
     }
 </script>
